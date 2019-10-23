2c1d770a01c110dfb0ad27cb254dd7eb99c2a6c1
Jeff Zinger <jzinger100@gmail.com>
2019-10-23 10:04:04 +0100

    state and getters

--- src/store.js
+++ src/store.js
@@ -5,7 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: { id: 'abc123', name: 'Adam Jahr' }
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+    todos: [
+      { id: 1, text: 'Cheese', done: true },
+      { id: 2, text: 'Bread', done: false },
+      { id: 3, text: 'Salami', done: true },
+      { id: 4, text: 'Aubergine', done: false }
+    ]
+  },
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: (state) => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    },
+    getEventById: (state) => (id) => {
+      return state.todos.find(event => event.id === id).text
+    }
   },
   mutations: {},
   actions: {}
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>Create Event, {{ $store.state.user }}</h1>
+  <div>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>This event is created by {{ user.name }}</p>
+    <p>There are {{ catLength }} items in this category.</p>
+    <p>There are {{ activeTodos }} active To Do's left</p>
+    <p>Remember to buy an {{ getEvent(4) }}</p>
+  </div>
 </template>
+
+<script>
+  import { mapState, mapGetters } from 'vuex'
+
+  export default {
+    computed: {
+      ...mapGetters({
+        catLength: 'catLength', 
+        activeTodos: 'activeTodosCount', 
+        getEvent: 'getEventById'
+        }),
+      ...mapState(['categories', 'user']) // <-- using object spread operator
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
