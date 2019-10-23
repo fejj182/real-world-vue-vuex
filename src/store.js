2f1653dbf478eb39d64458b52ef5e62e0ccad8ae
Jeff Zinger <jzinger100@gmail.com>
2019-10-23 11:51:46 +0100

    mutations and actions part 1

--- src/store.js
+++ src/store.js
@@ -6,28 +6,47 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     user: { id: 'abc123', name: 'Adam Jahr' },
-    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
     todos: [
       { id: 1, text: 'Cheese', done: true },
       { id: 2, text: 'Bread', done: false },
       { id: 3, text: 'Salami', done: true },
       { id: 4, text: 'Aubergine', done: false }
-    ]
+    ],
+    counter: 0
   },
   getters: {
     catLength: state => {
       return state.categories.length
     },
-    doneTodos: (state) => {
+    doneTodos: state => {
       return state.todos.filter(todo => todo.done)
     },
     activeTodosCount: (state, getters) => {
       return state.todos.length - getters.doneTodos.length
     },
-    getEventById: (state) => (id) => {
+    getEventById: state => id => {
       return state.todos.find(event => event.id === id).text
     }
   },
-  mutations: {},
-  actions: {}
+  actions: {
+    updateCount({ state, commit }, incrementBy) {
+      if (state.user) {
+        commit('INCREMENT_COUNT', incrementBy)
+      }
+    }
+  },
+  mutations: {
+    INCREMENT_COUNT(state, value) {
+      state.counter += value
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -5,22 +5,35 @@
     <p>There are {{ catLength }} items in this category.</p>
     <p>There are {{ activeTodos }} active To Do's left</p>
     <p>Remember to buy an {{ getEvent(4) }}</p>
+    <input type="number" v-model.number="incrementBy">
+    <button @click="incrementCount">Increment</button>
+    <p>Count = {{counter}}</p>
   </div>
 </template>
 
 <script>
-  import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
-  export default {
-    computed: {
-      ...mapGetters({
-        catLength: 'catLength', 
-        activeTodos: 'activeTodosCount', 
-        getEvent: 'getEventById'
-        }),
-      ...mapState(['categories', 'user']) // <-- using object spread operator
+export default {
+  data() {
+    return {
+      incrementBy: 2
     }
+  },
+  methods: {
+    incrementCount() {
+      this.$store.dispatch('updateCount', this.incrementBy)
+    }
+  },
+  computed: {
+    ...mapGetters({
+      catLength: 'catLength',
+      activeTodos: 'activeTodosCount',
+      getEvent: 'getEventById'
+    }),
+    ...mapState(['categories', 'user', 'counter'])
   }
+}
 </script>
 
 <style scoped>
